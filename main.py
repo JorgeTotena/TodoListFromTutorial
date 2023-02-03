01082a33db44edf7469473d9e5e5c892b7f062c2
ravenclawgt <jorge.totena@gmail.com>
2023-02-03 00:01:15 -0500

    Creating the input box and the data file.

--- main.py
+++ main.py
@@ -24,6 +24,26 @@ class todo:
         #Creates a list box within the root
         self.main_text = Listbox(self.root, height=9, bd=5, width=18, font="arial, 20 italic bold")
         self.main_text.place(x=280, y= 100)
+        
+        #The text function allows you to create an input box
+        self.text = Text(self.root, bd=5, height=2, width=25, font='arial, 10 bold')
+        self.text.place(x=20, y=120)
+    
+    #===============================ADD TASK ============================= #
+    
+    #
+    
+def add():
+    # The get method retrieves the text contained within the widget. It takes two arguments:
+    # 1.0 and END. These arguments specify the range of text to retrieve, where 
+    # 1.0 represents the first character of the first line and END represents 
+    # the end of the text in the widget.
+    content = self.text.get(1.0, END)
+    # Here we're inserting it into the listbox
+    self.main_text.insert(END, content)
+    #Opening the file 
+    with open('data.txt', "a") as file:
+        file.write(content)
     
 def main():
     # defines the main window of the program
